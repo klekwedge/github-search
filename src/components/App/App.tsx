5095853a26fff3126aaae921f671b5ef6b987e92
Andrey <antonov.andrey.10101@gmail.com>
2023-08-13 16:00:35 +0400

    feat: add UserTitle

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { observer } from 'mobx-react-lite';
 import Search from '../Search/Search';
 import Header from '../Header/Header';
 import Container from '../Container/Container';
 import defaultUser from '../../mock';
+import UserCard from '../UserCard/UserCard';
 
 const App = observer(() => {
   console.log('test');
+  const user = defaultUser;
 
 
   return (
@@ -18,7 +21,7 @@ const App = observer(() => {
         }}
         //  hasError={!user} onSubmit={fetchUser}
       />
-      {/* {user && <UserCard {...user} />} */}
+      {user && <UserCard {...user} />}
     </Container>
   );
 });
--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,36 +1,36 @@
+/* eslint-disable react/no-unused-prop-types */
 /* eslint-disable react/require-default-props */
 import { UserInfo, UserInfoProps } from '../UserInfo/UserInfo';
-// import { UserStat, UserStatProps } from 'components/UserStat';
-// import { UserTitle, UserTitleProps } from 'components/UserTitle';
+import { UserStat, UserStatProps } from '../UserStat/UserStat';
+import { UserTitle, UserTitleProps } from '../UserTitle/UserTitle';
 import classes from './UserCard.module.scss';
 
-// interface UserCardProps extends UserStatProps, UserTitleProps, UserInfoProps {
-//   avatar: string,
-//   bio?: string,
-// }
+interface UserCardProps extends UserStatProps, UserTitleProps, UserInfoProps {
+  avatar: string;
+  bio?: string;
+}
 
-function UserCard() {
-  // props: UserCardProps
+function UserCard({
+  login,
+  avatar,
+  created,
+  bio,
+  repos,
+  followers,
+  following,
+  blog,
+  company,
+  location,
+  twitter,
+}: UserCardProps) {
   return (
     <div className={classes.userCard}>
-      {/* <img
-      className={classes.avatar}
-      src={props.avatar}
-      alt={props.login}
-    />
-    <UserTitle
-      login={props.login}
-      name={props.name}
-      created={props.created}
-    />
-    <p className={`${classes.bio}${props.bio ? '' : ` ${classes.empty}`}`}>{props.bio || 'This profile has no bio'}</p>
-    <UserStat
-      repos={props.repos}
-      followers={props.followers}
-      following={props.following}
-    />
-    */}
-      {/* <UserInfo blog={props.blog} company={props.company} location={props.location} twitter={props.twitter} /> */}
+      <img className={classes.avatar} src={avatar} alt={login} />
+      <UserTitle login={login} name={name} created={created} />
+      <p className={`${classes.bio}${bio ? '' : ` ${classes.empty}`}`}>{bio || 'This profile has no bio'}</p>
+      <UserStat repos={repos} followers={followers} following={following} />
+
+      <UserInfo blog={blog} company={company} location={location} twitter={twitter} />
     </div>
   );
 }
