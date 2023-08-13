86c7cca815f401856cb29e383e54702b098f8f7a
Andrey <antonov.andrey.10101@gmail.com>
2023-08-13 16:06:48 +0400

    feat: add request

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,40 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { observer } from 'mobx-react-lite';
+import { useState } from 'react';
 import Search from '../Search/Search';
 import Header from '../Header/Header';
 import Container from '../Container/Container';
 import defaultUser from '../../mock';
 import UserCard from '../UserCard/UserCard';
+import isGithubUser from '../../utils/typeguards';
+import extractLocalUser from '../../utils/extract-local-user';
+import { GithubError, GithubUser, LocalGithubUser } from '../../types';
 
-const App = observer(() => {
-  console.log('test');
-  const user = defaultUser;
+const BASE_URL = 'https://api.github.com/users/';
 
+function App() {
+  const [user, setUser] = useState<LocalGithubUser | null>(defaultUser);
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+
+    const res = await fetch(url);
+    const gitHubUser = (await res.json()) as GithubUser | GithubError;
+
+    if (isGithubUser(gitHubUser)) {
+      setUser(extractLocalUser(gitHubUser));
+    } else {
+      setUser(null);
+    }
+  };
 
   return (
     <Container>
       <Header />
-      <Search
-        hasError={false}
-        onSubmit={() => {
-          console.log('s');
-        }}
-        //  hasError={!user} onSubmit={fetchUser}
-      />
+      <Search hasError={!user} onSubmit={fetchUser} />
       {user && <UserCard {...user} />}
     </Container>
   );
-});
+}
 
 export default App;
