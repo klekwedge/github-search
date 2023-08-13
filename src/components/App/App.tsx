50a5ebcd15f7ec9e77276961e825edf8262b2487
Andrey <antonov.andrey.10101@gmail.com>
2023-08-13 15:19:33 +0400

    feat: add InfoItem

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,25 @@
 import { observer } from 'mobx-react-lite';
-import Counter from '../counter';
-import Button from '../Button/Button';
 import Search from '../Search/Search';
 import Header from '../Header/Header';
+import Container from '../Container/Container';
+import defaultUser from '../../mock';
 
 const App = observer(() => {
   console.log('test');
 
-  const test = () => {};
 
   return (
-    <div>
+    <Container>
       <Header />
       <Search
         hasError={false}
         onSubmit={() => {
           console.log('s');
         }}
+        //  hasError={!user} onSubmit={fetchUser}
       />
-    </div>
+      {/* {user && <UserCard {...user} />} */}
+    </Container>
   );
 });
 
---
+++ src/mock/index.ts
@@ -0,0 +1,18 @@
+import { LocalGithubUser } from "../types";
+
+const defaultUser: LocalGithubUser = {
+  "login": "example",
+  "avatar": "https://avatars.githubusercontent.com/u/2121312312",
+  "name": "Nick Jonattan",
+  "company": "example",
+  "blog": "example.com",
+  "location": "example",
+  "bio": "React developer",
+  "twitter": "example",
+  "repos": 59,
+  "followers": 182,
+  "following": 5,
+  "created": "2022-03-01T22:22:33Z",
+};
+
+export default defaultUser;
